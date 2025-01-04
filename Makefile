1712b6cb69cc270c1f75d21d7545001e9133a79c
Jonnathan Lacerda Santos <jlacerda@student.42sp.org.br>
2025-01-04 18:09:43 -0300

    feat: add initial configuration main program and libraries

---
+++ Makefile
@@ -0,0 +1,48 @@
+NAME = push_swap
+
+SRCS_DIR = ./sources
+OBJS_DIR = ./objects
+INCLUDES_DIR = ./includes
+LIBS_DIR = ./libraries
+
+SOURCES = $(SRCS_DIR)/main.c
+
+OBJECTS = $(SOURCES:$(SRCS_DIR)/%.c=$(OBJS_DIR)/%.o)
+
+LIBFT_DIR = $(LIBS_DIR)/libft
+LIBFT = $(LIBFT_DIR)/libft.a
+
+PRINTF_DIR = $(LIBS_DIR)/printf
+PRINTF = $(PRINTF_DIR)/libftprintf.a
+
+CC = cc
+CFLAGS = -Wall -Wextra -Werror -I$(INCLUDES_DIR) -I$(LIBFT_DIR)/includes -I$(PRINTF_DIR)/includes
+
+all: $(LIBFT) $(PRINTF) $(NAME)
+
+$(LIBFT):
+	$(MAKE) -C $(LIBFT_DIR)
+
+$(PRINTF):
+	$(MAKE) -C $(PRINTF_DIR)
+
+$(NAME): $(OBJECTS) $(LIBFT) $(PRINTF) $(MLX)
+	$(CC) $(CFLAGS) $(OBJECTS) $(LIBFT) $(PRINTF) $(LDFLAGS) -o $@
+
+$(OBJS_DIR)/%.o: $(SRCS_DIR)/%.c
+	@mkdir -p $(OBJS_DIR)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJECTS)
+	$(MAKE) -C $(LIBFT_DIR) clean
+	$(MAKE) -C $(PRINTF_DIR) clean
+
+fclean: clean
+	rm -f $(NAME)
+	$(MAKE) -C $(LIBFT_DIR) fclean
+	$(MAKE) -C $(PRINTF_DIR) fclean
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ includes/push_swap.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlacerda <jlacerda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/04 17:25:59 by jlacerda          #+#    #+#             */
+/*   Updated: 2025/01/04 18:05:27 by jlacerda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include <stdlib.h>
+# include <unistd.h>
+# include "macros.h"
+# include "structs.h"
+
+#endif
